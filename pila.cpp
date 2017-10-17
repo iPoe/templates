7e6a1711121d9a494a85a59af9b57e080ce71a2d
Patrickgraham <leonardosawi12@gmail.com>
2017-10-17 11:25:01 -0500

    f2

--- pila.cpp
+++ pila.cpp
@@ -1,9 +1,57 @@
 #include <vector>
+#include <iostream>
 using namespace std;
-template <typename T>
-class Pila: public vector
+
+
+template <class T>
+class Pila: public vector<T>
 {
-	void adicionar (T elemento)
-	{
-	}
+	public:
+			Pila();
+			void adicionar(T elem);
+			T eliminar();
+			void mostrar();
+
+};
+
+template<class T>
+Pila<T>::Pila():vector<T>(){}
+
+template<class T>
+void Pila <T>::adicionar(T elem)
+{
+	this->push_back(elem);
+}
+
+template<class T>
+void Pila<T>::mostrar()
+{
+	int n= this->size();
+	for(int i=0; i<n; i++)
+		cout<<this->at(i)<<", ";
+	cout<<endl;
+}
+
+
+template <class T>
+T Pila <T>::eliminar () {
+	int posUltimo = this->size () - 1;
+	T elem = this->at (posUltimo);
+	this->pop_back();
+	return elem;
+}
+
+
+
+int main ()
+{
+	Pila <int> p;
+	p.adicionar (2);
+	p.adicionar (5);
+	p.adicionar (7);
+	
+	p.mostrar ();
+	
+	p.eliminar ();
+	p.mostrar ();
 }
